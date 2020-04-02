cf2d753645be1d7eaec5db03a3070ad21f581425
Scruffy Monster <dhirst1992@hotmail.com>
2020-04-02 10:37:28 +1000

    .

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Books.GraphQL.Entities
 {
+    [Table("Author")] 
     public class Author
     {
         [Key]
