70c688cf9d875ecb086c626584a862d25ae212ea
Scruffy Monster <dhirst1992@hotmail.com>
2020-04-02 10:31:15 +1000

    added schema

--- DbContexts/BooksContext.cs
+++ DbContexts/BooksContext.cs
@@ -1,13 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Books.GraphQL.Entities;
 
-namespace Books.GraphQL.DbContexts {
-    public class BooksContext: DbContext
+
+namespace Books.GraphQL.DbContexts
+{
+    public class BooksContext : DbContext
     {
-        public BooksContext(DbContextOptions<BooksContext> options): base(options)
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
         {
-            
+
         }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder){
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+        
+
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
     }
